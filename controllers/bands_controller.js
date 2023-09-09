791e0843dedf93f05694fe7c7e93a1c432d59cf7
saradonnelly97 <sara.donnelly8@gmail.com>
2023-09-09 12:03:36 -0400

    RESTFUL Routing for Creating/POST route completed

---
+++ controllers/bands_controller.js
@@ -0,0 +1,32 @@
+// DEPENDENCIES
+const bands = require('express').Router()
+const db = require('../models')
+const { Band } = db 
+
+// FIND A SPECIFIC BAND
+bands.get('/:id', async (req, res) => {
+    try {
+        const foundBand = await Band.findOne({
+            where: { band_id: req.params.id }
+        })
+        res.status(200).json(foundBand)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+// CREATE A BAND
+bands.post('/', async (req, res) => {
+    try {
+        const newBand = await Band.create(req.body)
+        res.status(200).json({
+            message: 'Successfully inserted a new band',
+            data: newBand
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// EXPORT
+module.exports = bands
